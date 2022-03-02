e27b99fb31a1104dadc867739403c86710c55355
RISHABH <gguptarishabh82@gmail.com>
2022-03-02 16:16:31 +0530

    Welcome To Line Comparison Problem

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+            Console.ReadLine();
+            UC1_CartesianSystem.Cartesion();
+            Console.ReadLine();
+            UC2_CompareLineLength.compareLine();
+            Console.ReadLine();
+            UC3_comparingLineSize.lineSize();
+            Console.ReadLine();
+
+        }
+    }
+}
